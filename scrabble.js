d178220db0fc7598ebd7b7206a8099a026c41f47
Abhinandan Mandal <105782385+abhinandan2540@users.noreply.github.com>
2024-10-29 17:01:22 +0530

    Add files via upload

---
+++ scrabble.js
@@ -0,0 +1,95 @@
+let opt1 = document.getElementById("p1");
+let opt2 = document.getElementById("p2");
+let opt3 = document.getElementById("p3");
+let opt4 = document.getElementById("p4");
+let opt5 = document.getElementById("p5");
+let opt6 = document.getElementById("p6");
+let opt7 = document.getElementById("p7");
+let opt8 = document.getElementById("p8");
+let opt9 = document.getElementById("p9");
+let opt10 = document.getElementById("p10");
+
+
+opt1.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "cat") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt2.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "football") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+
+opt3.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "hocky") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt4.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "bird") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt5.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "laptop") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt6.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "comodore") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt7.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "india") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt8.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "grain") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt9.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "real") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+opt10.addEventListener("click", () => {
+    let ask = prompt("ans : ");
+    if (ask == "yamaha") {
+        alert("correct ");
+    } else {
+        alert('wrong ');
+    }
+})
+
+
